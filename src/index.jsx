51e2b56fe1ed2c378ae81034612bcd2bf6360738
Jordan <Mrjordancollins@yahoo.com>
2025-04-19 16:11:28 -0400

    Try new Component Challenge
    Fail. Had to look up how to make components again.
    They are functions.

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,22 @@
+// It's really not necessary to rewrite all of this from scratch everytime since generating these things is more important but what ever.
+
 import { createRoot } from "react-dom/client";
-
+import Component from "./newComponent";
 const root = createRoot(document.getElementById("root"));
+function InLineComponent(){
+    return (
+        <div>
+            An inline component seems obvious now...
+            Hello from inline component.
+        </div>
+    )
+}
 
-root.render(<h1>Hello World</h1>)
+root.render(
+    <>
+    <h1>{"I wrote all this out by hand AGIAN! >:("}</h1>
+    <p>Well that was easier than I thought.</p>
+    <Component/>
+    <InLineComponent/>
+    </>
+)
---
+++ src/newComponent.jsx
@@ -0,0 +1,9 @@
+export default  function Component() {
+    return (
+        <div>
+            <p>There might not be anything more embarassing than not being able to remember basic shit about what you do for a living.</p>
+        </div>
+    )
+}
+
+
