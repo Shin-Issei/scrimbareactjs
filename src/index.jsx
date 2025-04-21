509ba73bba4932abb5a083d9f7803e7380b7ce9e
Jordan <Mrjordancollins@yahoo.com>
2025-04-21 17:01:14 -0400

    Realize: You are dogshit at styling
    You need a refresher on styling things
    because it isn't acceptable that you are
    incapable of basic flexbox maneuvers.
    Googling having to justify-content and
    slamming a footer to the bottom of a page
    really isn't becoming of a pro developer.
    Get the rust off your CSS skills and come back!

--- src/index.jsx
+++ src/index.jsx
@@ -1,34 +1,55 @@
 // Why the hell am I making and remaking the same bullshit over and over again?????
 
 import { createRoot } from "react-dom/client";
-import { Fragment } from "react";
 import reactLogo from "../src/react-logo.png";
-import "../src/index.css";
 const root = createRoot(document.getElementById("root"));
 root.render(<Page />);
 
+function Header() {
+  return (
+    <header className="header">
+      <img src={reactLogo} alt="React Logo" width="50vw" />
+      <nav>
+        <ul className="top-navigation">
+          <li>Price</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+function Main() {
+  return (
+    <main>
+      <h1>Reasons That You Are Worthy of Love! :D</h1>
+      <ul>
+        <li>Just about</li>
+        <li>Anything</li>
+        <li>Would be better than</li>
+        <li>Starting over</li>
+        <li>again</li>
+        <li>and again</li>
+        <li>and again</li>
+        <li>and again</li>
+        <li>and again</li>
+        <li>and again</li>
+      </ul>
+    </main>
+  );
+}
+function Footer() {
+  return <footer>© 2025 Collins All Rights Reserved</footer>;
+}
+
 function Page() {
   return (
-    <Fragment>
-      <header>
-        <img src={reactLogo} alt="React Logo" width="40px" />
-      </header>
-      <main>
-        <h1>Reasons That You Are Worthy of Love! :D</h1>
-        <ul>
-          <li>Just about</li>
-          <li>Anything</li>
-          <li>Would be better than</li>
-          <li>Starting over</li>
-          <li>again</li>
-          <li>and again</li>
-          <li>and again</li>
-          <li>and again</li>
-          <li>and again</li>
-          <li>and again</li>
-        </ul>
-        <footer>© 2025 Collins All Rights Reserved</footer>
-      </main>
-    </Fragment>
+    // I was so hype about the fragment thing too! :(
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
   );
 }
